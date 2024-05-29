4f9934c090e68037f91902cadee61c4b09b8a720
thenuts98 <juno9804@naver.com>
2024-05-29 17:00:38 +0900

    ~linked list

---
+++ 1_C/ch08_struct/ex03_struct_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct list
+{
+    int num;
+    struct list* next; 
+}LIST;
+
+int main(void){
+    struct list a = {10, 0}, b = {20, 0}, c = {30, 0};
+    struct list* head = &a;
+    struct list* current;
+    
+    current = head;
+    a.next = &b;
+    b.next = &c;
+
+    while(current != 0){
+        printf("%d\t", current->num);
+        current = current->next;
+    }
+    
+}
